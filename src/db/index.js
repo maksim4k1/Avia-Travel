300e4b233990aaa0fb2e633eb5bc42710d08bf32
Maksim Bazhenov <maksim4k1@gmail.com>
2021-09-08 11:16:28 +0600

    initial

---
+++ src/db/index.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    price: 14300,
+    name: "Ural Airlines",
+    wayMinutes: 390,
+    transfers: ["NDG", "MOW"]
+  },
+  {
+    id: 2,
+    price: 11200,
+    name: "Air Manas",
+    wayMinutes: 190,
+    transfers: []
+  },
+  {
+    id: 3,
+    price: 17800,
+    name: "Air Astana",
+    wayMinutes: 250,
+    transfers: ["TTH"]
+  },
+  {
+    id: 4,
+    price: 15500,
+    name: "S7",
+    wayMinutes: 230,
+    transfers: ["LLL", "TAI"]
+  },
+  {
+    id: 5,
+    price: 16350,
+    name: "Aeroflot",
+    wayMinutes: 255,
+    transfers: []
+  },
+  {
+    id: 6,
+    price: 15300,
+    name: "Turkish Airlines",
+    wayMinutes: 512,
+    transfers: []
+  }
+];
