45b1916587dcc71e834b5ab00fb85aa461fcabd5
Maksim Bazhenov <maksim4k1@gmail.com>
2021-09-13 11:46:09 +0600

    deploy project to gh-pages

--- src/pages/errors/404.jsx
+++ src/pages/errors/404.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { URL } from "../../routers";
 import { gap } from "../../styles/mixins";
 import style from "./Error404.module.scss";
 
@@ -26,7 +27,7 @@ function Error404 () {
     <Content>
       <Error>404</Error>
       <ErrorText>Page not found!</ErrorText>
-      <ErrorLink>Go to <NavLink to="/" className={style.link}>main page</NavLink></ErrorLink>
+      <ErrorLink>Go to <NavLink to={`${URL}/`} className={style.link}>main page</NavLink></ErrorLink>
     </Content>
   );
 }
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 import Error404 from "../pages/errors/404";
 import Main from "../pages/Main";
 
+export const URL = "/Avia-Travel";
+
 const routers = [
-  {path: "/", component: Main, exact: true},
-  {path: "/error/404", component: Error404, exact: true},
+  {path: `${URL}/`, component: Main, exact: true},
+  {path: `${URL}/error/404`, component: Error404, exact: true},
 ]
 
 export default routers;
